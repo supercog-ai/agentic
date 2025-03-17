98c8639160932dd7fc8a139a4cdcbf7588a18f54
ritzz26 <ritagarwalrit@gmail.com>
2025-03-17 12:05:53 -0700

    fixed example_test

--- tests/example_test.py
+++ tests/example_test.py
@@ -2,8 +2,3 @@ import pytest
 
 def test_non_llm_function():
     assert 1 + 1 == 2
-
-@pytest.mark.requires_llm
-def test_function_requiring_llm():
-    result = call_to_llm_api("test prompt")
-    assert result is not None
